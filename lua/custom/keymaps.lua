d7cff1bcfa3bea7a5285e836f5eeaaeef70175e3
atilla <atillanedim42@gmail.com>
2026-01-19 20:31:48 +0100

    new shortcuts

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -60,6 +60,13 @@ map("n", "<M-v>", "<cmd>vsplit<cr>", { desc = "Vertical Split" })
 map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy.nvim" })
 map("n", "<leader>w", "<cmd>w<cr>", { desc = "Save" })
 map("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit" })
+-- Undo/Redo on familiar shortcuts
+map({ "n", "v" }, "<C-z>", "u", opts)
+map("i", "<C-z>", "<C-o>u", opts)
+map("c", "<C-z>", "<Nop>", opts) -- prevent shell suspend
+map("t", "<C-z>", "<Nop>", opts) -- prevent terminal job suspend
+map({ "n", "v" }, "<C-y>", "<C-r>", opts)
+map("i", "<C-y>", "<C-o><C-r>", opts)
 
 --------------------------------------------------------------------
 -- VISUAL MODE
@@ -71,6 +78,15 @@ map("v", ">", ">gv", opts)
 -- INSERT MODE
 --------------------------------------------------------------------
 map("i", "jk", "<Esc>", opts)
+-- Delete from cursor to end of current word (Ctrl+Delete)
+map("i", "<C-Del>", "<C-o>de", { desc = "Delete word forward" })
+-- Same for Ctrl+Enter (if terminal sends <C-CR>)
+map("i", "<C-CR>", "<C-o>de", { desc = "Delete word forward" })
+-- Delete previous word (Ctrl+Backspace); fallback to <C-h> if terminal uses that
+map("i", "<C-BS>", "<C-w>", { desc = "Delete word backward" })
+map("i", "<C-h>", "<C-w>", { desc = "Delete word backward (fallback)" })
+-- Some terminals send <BS> (0x7f) even with Ctrl held
+map("i", "<BS>", "<C-w>", { desc = "Delete word backward (BS 0x7f)" })
 
 
 --------------------------------------------------------------------
@@ -85,4 +101,3 @@ vim.keymap.set("t", "<M-CR>", "<C-\\><C-n><C-w>w", { desc = "Focus Editor Window
 -- Resize window width
 vim.keymap.set("n", "<M-C-]>", "<cmd>vertical resize +5<cr>", { desc = "Increase Window Width" })
 vim.keymap.set("n", "<M-C-[>", "<cmd>vertical resize -5<cr>", { desc = "Decrease Window Width" })
-
